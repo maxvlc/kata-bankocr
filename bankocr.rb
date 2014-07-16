cc116ecebeea2d1b81d929e2572b97c256f8651e
maxvlc <maxcar@gmail.com>
2014-07-16 13:25:35 +0200

    BankOCR scanner detects a 2 REFAC

--- bankocr.rb
+++ bankocr.rb
@@ -1,21 +1,22 @@
 class BankOCR
 
+  CHARACTERS = {0=> " _ "+
+                    "| |"+
+                    "|_|"+
+                    "   ",
+                1=> "   "+
+                    "  |"+
+                    "  |"+
+                    "   ",
+                2=> " _ "+
+                    " _|"+
+                    "|_ "+
+                    "   ",
+              }
+
   def self.read(scan)
-    zero =  " _ "+
-            "| |"+
-            "|_|"+
-            "   "
-    one =   "   "+
-            "  |"+
-            "  |"+
-            "   "
-    two =   " _ "+
-            " _|"+
-            "|_ "+
-            "   "
-    return 0 if scan.include? zero
-    return 1 if scan.include? one
-    return 2 if scan.include? two
+    CHARACTERS.select {|key,value| return key if value==scan}
+  
   end
 
 end
