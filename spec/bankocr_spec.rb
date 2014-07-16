06d50af2ffab468e9539347e75872f4c871ba2b1
maxvlc <maxcar@gmail.com>
2014-07-16 13:34:04 +0200

    BankOCR moving forward to 8 GREEN

--- spec/bankocr_spec.rb
+++ spec/bankocr_spec.rb
@@ -18,12 +18,60 @@ describe "BankOCR scanner" do
 		expect(BankOCR.read(one)).to eq 1
 	end
 
-		it "detects a 2" do
+	it "detects a 2" do
 		two = " _ "+
 					" _|"+
 					"|_ "+
 					"   "
 		expect(BankOCR.read(two)).to eq 2
+	end
+
+	it "detects a 3" do
+		two = " _ "+
+					" _|"+
+					" _|"+
+					"   "
+		expect(BankOCR.read(two)).to eq 3
+	end
+
+	it "detects a 4" do
+		two = "   "+
+					"|_|"+
+					"  |"+
+					"   "
+		expect(BankOCR.read(two)).to eq 4
+	end
+	
+	it "detects a 5" do
+		two = " _ "+
+					"|_ "+
+					" _|"+
+					"   "
+		expect(BankOCR.read(two)).to eq 5
+	end
+	
+	it "detects a 6" do
+		two = " _ "+
+					"|_ "+
+					"|_|"+
+					"   "
+		expect(BankOCR.read(two)).to eq 6
+	end
+	
+	it "detects a 7" do
+		two = " _ "+
+					"  |"+
+					"  |"+
+					"   "
+		expect(BankOCR.read(two)).to eq 7
+	end
+	
+	it "detects a 8" do
+		two = " _ "+
+					"|_|"+
+					"|_|"+
+					"   "
+		expect(BankOCR.read(two)).to eq 8
 	end
 
 end
